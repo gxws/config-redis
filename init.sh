ee2b009825a88f666e54bb5733a6516273bc1bb5
朱伟亮 <zhuwl120820@gxwsxx.cim>
2015-04-01 15:05:42 +0800

    修复编译路径

--- init.sh
+++ init.sh
@@ -8,17 +8,19 @@ wget -P $redis_base http://software.gxwsxx.com:18000/software/redis.tar.gz
 mkdir $redis_base/tmp
 tar -zxf $redis_base/redis.tar.gz -C $redis_base/tmp
 rm -r $redis_base/redis.tar.gz
-if [ -e $redis_base/redis ]; then
-    rm -fr $redis_base/redis
+if [ -e $redis_worker_base ]; then
+    rm -fr $redis_worker_base
 fi
 for temp in $redis_base/tmp;
 do
-	mv -f $temp $redis_base/redis
+	mv -f $redis_worker_base
 done
 rm -fr $redis_base/tmp
 
 echo "编译安装redis"
+cd $redis_worker_base
 make && make install
+cd -
 
 rm -rf $redis_base/${redis_port}*
 
